833e799463affb0195d305d2c9e03ae4da67f2be
Daru <radies@rambler.ru>
2025-02-15 08:13:19 +0300

    Форма Работ

--- front-service/src/main/resources/static/work/work.js
+++ front-service/src/main/resources/static/work/work.js
@@ -108,6 +108,11 @@ angular.module('market').controller('workController', function ($scope, $http, $
         $scope.WorkId = workId;
         $location.path('/worktime');
     }
-
+    window.onload = function() {
+        document.getElementById('myLinkId').addEventListener('click', function(event) {
+            event.preventDefault();
+            // Функционал обработки клика
+        });
+    };
     $scope.loadWork();
 })
