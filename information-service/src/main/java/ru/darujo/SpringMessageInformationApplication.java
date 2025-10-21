6c66c741dc60638d2f91d2de882b4b0a9616e49c
Daru <radies@rambler.ru>
2025-10-22 00:02:42 +0300

    1) information service init

---
+++ information-service/src/main/java/ru/darujo/SpringMessageInformationApplication.java
@@ -0,0 +1,13 @@
+package ru.darujo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMessageInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMessageInformationApplication.class, args);
+	}
+
+}
