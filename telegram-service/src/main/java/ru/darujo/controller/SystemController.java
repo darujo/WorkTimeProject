77efd40a540d5881fbb1883aba941748d190dec1
Daru <radies@rambler.ru>
2025-12-26 09:50:49 +0300

    1) Мониторинг и остановка процесса

--- telegram-service/src/main/java/ru/darujo/controller/SystemController.java
+++ telegram-service/src/main/java/ru/darujo/controller/SystemController.java
@@ -1,11 +1,14 @@
 package ru.darujo.controller;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,25 +19,30 @@ import ru.darujo.telegram_bot.TelegramBot;
 @RestController()
 @RequestMapping("/v1/bot")
 @Log4j2
-public class SystemController implements ApplicationContextAware {
-    private TelegramBot telegramBot;
+public class SystemController {
+    //    private TelegramBot telegramBot;
+//
+//    @Autowired
+//    public void setTelegramBot(TelegramBot telegramBot) {
+//        this.telegramBot = telegramBot;
+//    }
+    private ApplicationContext applicationContext;
 
     @Autowired
-    public void setTelegramBot(TelegramBot telegramBot) {
-        this.telegramBot = telegramBot;
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
     }
 
     @GetMapping("/test")
-    public void test() {}
-    private ApplicationContext context;
-    @PostMapping("/shutdownContext")
-    public void shutdownContext() {
-        telegramBot.close();
-        ((ConfigurableApplicationContext) context).close();
+    public void test() {
     }
 
-    @Override
-    public void setApplicationContext(ApplicationContext ctx) throws BeansException {
-        this.context = ctx;
+    @PostMapping("/shutdownContext")
+    @Async
+    public void shutdownContext() {
+//        telegramBot.close();
+        SpringApplication.exit(applicationContext);
+//        ((ConfigurableApplicationContext) context).close();
+    }
 
-    }}
+}
