5a0c290848c91c61541212150684f9cece8e2ef4
Daru <radies@rambler.ru>
2026-05-22 23:40:03 +0300

    1) backup db

--- update-service/src/main/java/ru/darujo/service/ScheduleService.java
+++ update-service/src/main/java/ru/darujo/service/ScheduleService.java
@@ -29,7 +29,7 @@ public class ScheduleService implements AutoCloseable {
 
     @PostConstruct
     private void init() {
-        executor.schedule(taskService.getBackUpTask(), 10, TimeUnit.SECONDS);
+//        executor.schedule(taskService.getBackUpTask(), 10, TimeUnit.SECONDS);
         executor.scheduleAtFixedRate(taskService.getBackUpTask(), DateHelper.getStartTime(23, 0), 24L * 86400, TimeUnit.SECONDS);
         executor.scheduleAtFixedRate(taskService.getTaskAvailService(), 5, 2, TimeUnit.MINUTES);
 //        addUpdate(new ArrayList<>());
