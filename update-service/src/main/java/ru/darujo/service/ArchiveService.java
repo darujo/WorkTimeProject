cb6baf32f29142a1ad6013de19de2a5c671d3024
Daru <radies@rambler.ru>
2026-06-12 13:07:32 +0300

    1) архиватор 7z

--- update-service/src/main/java/ru/darujo/service/ArchiveService.java
+++ update-service/src/main/java/ru/darujo/service/ArchiveService.java
@@ -1,10 +1,10 @@
 package ru.darujo.service;
 
 import jakarta.annotation.PostConstruct;
-import org.apache.commons.compress.archivers.sevenz.SevenZArchiveEntry;
-import org.apache.commons.compress.archivers.sevenz.SevenZFile;
+import org.apache.commons.compress.archivers.sevenz.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.tukaani.xz.LZMA2Options;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -12,21 +12,42 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
-public class ArhiveService {
-    public void dddd() {
-//        try (SevenZOutputFile out = new SevenZOutputFile(new File("archive.7z"))) {
-//            SevenZArchiveEntry entry = out.createArchiveEntry(fileToArchive, name);
-//            out.putArchiveEntry(entry);
-//            out.write(contentOfEntry);
-//            out.closeArchiveEntry();
-//        }
+public class ArchiveService {
+    public void packArh() {
+        try (SevenZOutputFile out = new SevenZOutputFile(new File(pathFile + "/archive_new.7z"))) {
+            List<SevenZMethodConfiguration> methods = new ArrayList<>();
+
+
+            LZMA2Options lzma2Options = new LZMA2Options();
+            lzma2Options.setPreset(LZMA2Options.PRESET_MAX);
+            SevenZMethodConfiguration lzmaConfig =
+                    new SevenZMethodConfiguration(SevenZMethod.LZMA2, lzma2Options);
+            methods.add(lzmaConfig);
+            out.setContentMethods(methods);
+            SevenZArchiveEntry entry = out.createArchiveEntry(new File("new"), "new");
+            entry.setDirectory(true);
+            out.putArchiveEntry(entry);
+            out.closeArchiveEntry();
+
+            entry = out.createArchiveEntry(new File("new", "input.txt"), "input.txt");
+
+            out.putArchiveEntry(entry);
+            out.write("Текст файла".getBytes());
+            out.closeArchiveEntry();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @PostConstruct
     private void unpackArh() {
+        packArh();
         unpackArh(null);
+
     }
 
     @Value("${update.save-into}")
@@ -34,7 +55,7 @@ public class ArhiveService {
 
     private void unpackArh(String pas) {
         Path outputDir = Path.of(pathFile);
-        try (final SevenZFile sevenZFile = SevenZFile.builder().setFile(new File(pathFile + "/archive.7z")).setPassword(pas == null ? null : pas.getBytes(StandardCharsets.UTF_8)).get()) {
+        try (final SevenZFile sevenZFile = SevenZFile.builder().setFile(new File(pathFile + "/archive_new.7z")).setPassword(pas == null ? null : pas.getBytes(StandardCharsets.UTF_8)).get()) {
 // todo только 7 степень сжатия
             SevenZArchiveEntry sevenZArchiveEntry;
             sevenZArchiveEntry = sevenZFile.getNextEntry();
